11501702357376729539ef6cff9d50780daa36d1
Dominic M <spartanbloodmoreno@gmail.com>
2024-08-02 12:58:11 -0500

    Stopwatch V2

---
+++ stopWatchProjectV2/app.js
@@ -0,0 +1,27 @@
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+let displayTime = document.getElementById('displayTime');
+let timer = null;
+
+function stopwatch() {
+    seconds++;
+    if(seconds == 60){
+        seconds = 0;
+        minutes++;
+
+        if(minutes == 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    displayTime.innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+
+function watchStart(){
+    if(timer !== null) {
+        clearInterval(timer);
+    }
+    timer.setInterval(stopwatch, 1000);
+}
